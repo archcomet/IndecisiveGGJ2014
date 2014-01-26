7c32c502ff03a1681b13f7a8045a7ca32689d590
michaelskiles <skiles85@gmail.com>
2014-01-26 14:13:36 -0800

    removing unnecessary configure

--- public/src/app/systems/promptSystem.js
+++ public/src/app/systems/promptSystem.js
@@ -12,11 +12,6 @@ define([
             animateCount: 0
         },
 
-        configure: function () {
-
-            this.animatePrompt();
-        },
-
         update: function () {
 
             this.animatePrompt()
